73fbfdd8ec7147b2bcbdb151af402f80a6057381
rikhil7 <rikhil.taneja@kalvium.community>
2023-12-26 14:43:59 +0530

    update

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <HomeComponent />
-      {/* <QuizComponent /> */}
-      {/* <ResultComponent /> */}
+      <QuizComponent />
+      <ResultComponent />
     </div>
   );
 }
